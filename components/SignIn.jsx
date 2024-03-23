fc3ad641c86cd59ece3efab32a7a2fe2549b6ff0
ehye <yjw9610@hotmail.com>
2024-03-23 15:08:10 +0800

    10.8

--- components/SignIn.jsx
+++ components/SignIn.jsx
@@ -1,9 +1,48 @@
-import { Text, View } from 'react-native'
+import { Text, TextInput, Pressable, View } from 'react-native'
+import { useFormik } from 'formik'
+import theme from '../theme'
 
-const SignIn = () => (
-  <View>
-    <Text>The sign-in view</Text>
-  </View>
-)
+const onSubmit = (values) => {
+  console.log(values)
+}
+
+const initialValues = {
+  username: '',
+  password: '',
+}
+
+const SignIn = () => {
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+  })
+
+  return (
+    <View style={{ alignSelf: 'center' }}>
+      <TextInput
+        placeholder="Username"
+        value={formik.values.username}
+        onChangeText={formik.handleChange('username')}
+      />
+      <TextInput
+        placeholder="Password"
+        value={formik.values.password}
+        onChangeText={formik.handleChange('password')}
+      />
+      <Pressable onPress={formik.handleSubmit}>
+        <Text
+          style={{
+            backgroundColor: theme.colors.primary,
+            color: '#ffffff',
+            borderRadius: 4,
+            padding: '0 4px 2px 4px',
+          }}
+        >
+          Sign In
+        </Text>
+      </Pressable>
+    </View>
+  )
+}
 
 export default SignIn
